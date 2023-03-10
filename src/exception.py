3b4d7957eb295029e8623cc11e1570c8fef5a128
unknown <>
2023-03-10 19:13:14 +0300

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,5 +17,5 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
-    
+
 
